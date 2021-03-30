1600ed337547900d458e32f97a5a506842b4ee58
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-29 23:25:39 -0300

    :sparkles: Adicionando super classe Pessoa

---
+++ Atividade_1/Pessoa.java
@@ -0,0 +1,13 @@
+package Atividade_1;
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    protected String getNome() {
+        return nome;
+    }
+    
+}
