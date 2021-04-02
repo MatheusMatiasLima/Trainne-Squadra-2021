515a79d96b9db381c061564521a797e85451e64e
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-04-02 19:29:15 -0300

    :coffin: tirando as linhas que não estavam sendo utilizadas na main

--- Atividade_1/Programa.java
+++ Atividade_1/Programa.java
@@ -2,18 +2,8 @@ package Atividade_1;
 
 public class Programa {
     public static void main(String[] args) {
-        Restaurante meuRestaurante = new Restaurante();
-        //meuRestaurante.gerarDadosAleatorios();
-        //meuRestaurante.imprimirCozinhas();
         Interface rest = new Interface();
         rest.exibir();
-        
-        //ManipuladoraArquivos ma = new ManipuladoraArquivos();
-        //Manipuladora.gerarArquivoBinario(meuRestaurante.getCozinhas(), "binario");
-
-        //meuRestaurante = null;
-        //meuRestaurante = Manipuladora.lerArquivoBinario("binario");
-        //meuRestaurante.imprimirCozinhas();
 
     }
 }
